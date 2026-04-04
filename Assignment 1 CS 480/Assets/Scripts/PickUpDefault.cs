744c04762868c6511fcac380ec6ce619a0c6630b
Joshua Henrikson <Grimmknight2005@gmail.com>
2026-04-03 19:01:06 -0700

    Implemented basic pickups that give player a defined # of points, also added player score tracking and UI

---
+++ Assignment 1 CS 480/Assets/Scripts/PickUpDefault.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PickUpDefault : MonoBehaviour
+{
+    //PickUp BaseClass have other pickups inharet this behavoir
+    public int points = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assignment 1 CS 480/Assets/Scripts/PlayerController.cs
+++ Assignment 1 CS 480/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using Unity.VisualScripting;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine.InputSystem;
 using UnityEngine;
 
@@ -11,9 +12,12 @@ public class PlayerController : MonoBehaviour
     private float moveX; //X Movement variable
     private float moveY; //Y Movement variable
     public float playerSpeed = 5;//Speed of character movement Default 5
+    public TextMeshProUGUI countText; //Referance to Score UI element
+    private int playerPoints; //Storing score per player
     void Start()
     {
         rb = GetComponent<Rigidbody>(); //Uses GetComponent to set rd to Rigidbody component
+        setPlayerScore();// update UI
     }
     void OnMove(InputValue movementValue)//On any movement?
     {
@@ -21,10 +25,23 @@ public class PlayerController : MonoBehaviour
         moveX = movementVector.x; // extract x from movementVector (x,y) make avalable to rest of code
         moveY = movementVector.y; // extract y from movementVector (y,x) make avalable to rest of code 
     }
-    void Update()
+    void setPlayerScore()//Displays player's score on UI
     {
-
+        countText.text = "Score: " + playerPoints.ToString();
     }
+    void OnTriggerEnter(Collider other)//execute once on trigger
+    {
+        if(other.gameObject.CompareTag("PickUp"))//If pickup
+        {
+            other.gameObject.SetActive(false); //disable pickup
+            playerPoints += other.gameObject.GetComponent<PickUpDefault>().points;//chack pickups point value stored in PickUpDefault script
+            print("PlayerPoints: " + playerPoints);//Debug
+            setPlayerScore();// update UI
+
+        }
+    }
+        
+
     void FixedUpdate()//Fixed interval update ensures physics is consistant regaurdless of framerate
     {
         //Construct movement vector3
